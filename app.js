1ef89ad85ea239972f82097c1669a3c4926eddd1
Damon Self <damongself@gmail.com>
2020-04-04 19:08:19 -0400

    Installs yargs package and writes test functions
    After installing the yargs package, I wrote an add, remove, read, and
    list command for corresponding CLI input. They log appropriate strings
    to the console currently.

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-getNotes();
 
-console.log(chalk.red('Error!'));
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log(chalk.green.bold('Adding a new note!'))
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Removes a note',
+  handler: function () {
+    console.log(chalk.red.bold('Removing a note!'))
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Reads a note',
+  handler: function () {
+     console.log(chalk.blue.bold('This is a note!'))
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'lists out all notes',
+  handler: function () {
+    console.log(chalk.yellow.bold('Here are all the notes:'))
+  }
+});
+
+// add, remove, read, list
+
+console.log(yargs.argv);
