34334ad13d035bec855f03c73c1dd6220c8ac101
Damon Self <damongself@gmail.com>
2020-04-04 20:48:10 -0400

    Updates app and notes files and instantiates notes.json
    The notes.js file now contains multiple methods used for adding a note
    to our notes.json file. The addNote function is called in app.js when
    a user calls the add command with a unique title and body provided.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const { getNotes, addNote } = require('./notes.js');
 
 
 // Create add command
@@ -20,8 +20,7 @@ yargs.command({
     }
   },
   handler: function (argv) {
-    console.log(`Title: ${argv.title}`);
-    console.log(`Content: ${argv.body}`);
+    addNote(argv.title, argv.body);
   }
 });
 
--- notes.js
+++ notes.js
@@ -1,4 +1,45 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
 const getNotes = function () {
   console.log('Your notes...');
 };
-module.exports = getNotes;
+
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  if(notes.filter(note => note.title === title).length > 0) {
+    console.error(chalk.red('That title is already in use! Try a different title or delete the old one.'));
+    return [];
+  } else {
+
+    notes.push({
+      title: title,
+      body: body 
+    });
+  
+    saveNotes(notes);
+    console.log(chalk.green('New note created!'));
+  };
+};
+
+const saveNotes = function (notes) {
+  const newJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', newJSON);
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+  
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    console.error(chalk.red("File is nonexistent, empty, or doesn't contain JSON"));
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+};
